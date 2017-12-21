b27aa404f6920680dbaa10def7bfa9ea376c11ca
Holger Pfaff <holger.pfaff@gmx.net>
2017-12-21 11:48:39 +0100

    Modified year in footer

--- app/footer.php
+++ app/footer.php
@@ -2,8 +2,8 @@
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
-                <h5>Acrylbilder von 2013 bis 2016</h5>
-                <p>Die Bilder wurden alle zwischen 2013 und 2016 auf Leinwand gemalt. Die ersten Bilder waren noch 30x40 cm, die späteren sind meist in größeren Formaten bis 100x80 cm gehalten.</p>
+                <h5>Acrylbilder von 2013 bis 2017</h5>
+                <p>Die Bilder wurden alle zwischen 2013 und 2017 auf Leinwand gemalt. Die ersten Bilder waren noch 30x40 cm, die späteren sind meist in größeren Formaten bis 100x80 cm gehalten.</p>
             </div>
             <div class="col l4 offset-l2 s12">
                 <h5>Links</h5>
@@ -17,11 +17,11 @@
 
     <div class="footer-copyright grey darken-3 grey-text text-lighten-1">
         <div class="container hide-on-small-only">
-            <i>© 2016 Holger Pfaff, Ostlandweg 1, 31848 Bad Münder, webmaster@holger-pfaff.de</i>
+            <i>© 2017 Holger Pfaff, Ostlandweg 1, 31848 Bad Münder, webmaster@holger-pfaff.de</i>
         </div>
         <div class="container hide-on-med-and-up">
             <ul>
-                <li><i>© 2016 Holger Pfaff</i></li>
+                <li><i>© 2017 Holger Pfaff</i></li>
                 <li><i>Ostlandweg 1</i></li>
                 <li><i>31848 Bad Münder</i></li>
                 <li><i>webmaster@holger-pfaff.de</i></li>
